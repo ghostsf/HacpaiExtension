966b097763e7ef3b6a96aaa76c13907b8476d1fa
ghostsf <ghost_sf@163.com>
2020-03-25 22:59:30 +0800

    chore: first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+<h1 align="center">Hacpai Extension</h1>
+
+> Hacpai Extension 是 [黑客派](https://hacpai.com/) 的浏览器扩展
+> 黑客派 - 为未来而构建的开发者社区，找到你所需的技术解决方案、记录和分享我们的程序人生
+
+## 介绍
+
+> 主要是为了了解下浏览器扩展的开发，就先简单弄了几个功能
+
++ 自动签到
++ 消息提醒
++ 双击返回顶部
+
+## 安装
+
+<a href="https://chrome.google.com/webstore/detail/ajmjbhndjfmegheielkjcmmgddkaghee"><img src="https://raw.githubusercontent.com/alrra/browser-logos/master/src/chrome/chrome_128x128.png" width="48" /></a>
+
+## 开发
+
+**Chrome安装扩展：**
+
+打开 `chrome://extensions/`
+
+打开右上角 `开发者模式`
+
+点击左上角 `加载已解压的扩展程序`
+
+选择源码目录
+
+## Author
+
+**Hacpai Extension** © [ghostsf](https://github.com/ghostsf), Released under the [MIT](./LICENSE) License.<br>
+
+> Blog [@ghostsf](https://ghostsf) · GitHub [@ghostsf](https://github.com/ghostsf) · Telegram [@ghostsf](https://t.me/ghostsf)
---
+++ background.js
@@ -0,0 +1,155 @@
+const storage = chrome.storage.sync;
+const hacpaiHost = 'https://hacpai.com';
+
+function browser_notifications_create(id, options) {
+    chrome.notifications.clear(id);  
+    chrome.notifications.create(id, options);
+}
+
+// 定时任务
+chrome.alarms.create("checkMsg", {periodInMinutes: 3});
+chrome.alarms.create("autoMission", {periodInMinutes: 30});
+
+chrome.alarms.onAlarm.addListener(function( a ){
+    switch (a.name){
+    case "checkMsg":
+        storage.get(function (response) {
+            Number(response.newMsg) && checkMsg();
+        });
+        break;
+    case "autoMission":
+        storage.get(function (response) {
+            Number(response.autoMission) && autoMission();
+        });
+        break;
+    }
+});
+
+// 通知设置
+chrome.notifications.onClicked.addListener(function(notificationId){
+    switch (notificationId){
+        case "newMsg":
+            clean_msg();
+            break;
+        case "autoMission":
+            chrome.tabs.create({url: hacpaiHost + "/activity/checkin"});
+            break;
+    }
+    chrome.notifications.clear(notificationId);
+});
+
+// 自动签到
+function autoMission(){
+    console.log("autoMission")
+    storage.get(async function (response) {
+        if( response.autoMission == new Date().getUTCDate() ){
+            console.log('今天已经成功领取积分奖励了');
+            return;
+        }
+
+        chrome.cookies.get({
+            url : hacpaiHost,
+            name : 'symphony'
+        }, function(cookies) {
+            if (cookies && cookies.value) {
+                $.ajax({
+                    url: hacpaiHost + "/activity/daily-checkin",
+                    success: function(data){
+                        var html = $.trim(data);
+                        var signUrl = $(html).find('.btn.green').attr('href');
+                        console.log('signUrl:'+signUrl)
+                        var nowTime  =  new  Date();
+                        $.ajax({
+                            url: signUrl,
+                            success: function(data){
+                                var html = $.trim(data);
+                                var code  =  $(html).find('.vditor-reset code:first').text();
+                                storage.set( {"autoMission" : new Date().getUTCDate()} );
+                                var msg = "签到成功！\n获得积分："+code+" \n签到时间："+nowTime.toLocaleTimeString();                          
+                                browser_notifications_create(
+                                    "autoMission" ,
+                                    {
+                                        type    : "basic",
+                                        iconUrl : "icon/icon_notice.png",
+                                        title   : "HacpaiExtension 提醒您",
+                                        message : msg,
+                                    }
+                                );
+                            },
+                            error: function(err){
+                                console.log(nowTime.toLocaleTimeString()  +  " 签到请求失败\n"  +  err);
+                            }
+                        });
+                    },
+                    error: function(){
+                        console.log("请求失败！");
+                    }
+                });
+            }else{
+                console.log('未登录')
+            }
+        });
+    });
+
+}
+
+
+//清除通知图标，打开通知地址
+function clean_msg(){
+    chrome.browserAction.setBadgeText({
+        text: ''
+    });
+    browser.tabs.create({url: hacpaiHost + "/notifications/commented"});
+}
+
+chrome.browserAction.setBadgeBackgroundColor({
+    color: '#FF2800',
+});
+
+chrome.browserAction.setBadgeTextColor &&
+    chrome.browserAction.setBadgeTextColor({
+        color: '#fff',
+    });
+
+// 消息通知
+function checkMsg(){
+    console.log("checkMsg")
+    chrome.cookies.get({
+        url : hacpaiHost,
+        name : 'symphony'
+    }, function(cookies) {
+        if (cookies && cookies.value) {
+            chrome.browserAction.setIcon({path: "icon/icon.png"});
+            $.ajax({
+                url: hacpaiHost + "/api/v2/notifications/unread/count",
+                success: function(res){
+                    if(res.sc == 0) {
+                        var count = 0;
+                        for(key in res.data) {
+                            count += res.data[key];
+                        }
+                        chrome.browserAction.setBadgeText({
+                            text: count
+                        });
+                        browser_notifications_create(
+                            "newMsg" ,
+                            {
+                                type       : "basic",
+                                iconUrl    : "icon/icon_notice.png",
+                                title      : "HacpaiExtension 提醒您",
+                                message    : "您有黑客派社区的"+count+"条未读新消息，点击查看。"
+                            });
+                    }else{
+                        console.log("请求失败！ " + res.msg);
+                    }   
+                },
+                error: function(){
+                    console.log("请求失败！");
+                }
+            });
+        }else{
+            chrome.browserAction.setIcon({path: "icon/icon_nologin.png"});
+            console.log('未登录')
+        }
+    });
+}
